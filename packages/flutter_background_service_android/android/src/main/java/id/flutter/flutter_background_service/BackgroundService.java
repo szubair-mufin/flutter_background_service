e2a2f29ecd0f39d8164ad59c2c3cd7c593f1f052
Zubair Ahmed Soomro <158747313+szubair-mufin@users.noreply.github.com>
2024-12-09 15:14:10 +0500

    Changed to sticky start

--- packages/flutter_background_service_android/android/src/main/java/id/flutter/flutter_background_service/BackgroundService.java
+++ packages/flutter_background_service_android/android/src/main/java/id/flutter/flutter_background_service/BackgroundService.java
@@ -190,7 +190,7 @@ public class BackgroundService extends Service implements MethodChannel.MethodCa
         WatchdogReceiver.enqueue(this);
         runService();
 
-        return START_NOT_STICKY;
+        return START_STICKY;
     }
 
     @SuppressLint("WakelockTimeout")
